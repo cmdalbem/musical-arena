e9f7377a8ec7048e9e74376cb30d2a930a31c2e8
ninja.dalbem <ninja.dalbem@9ff2b497-3b3b-2cb2-345e-040a1e5cde19>
2010-10-24 23:10:01 +0000

    Interpreting musics properly. I created a simple printing system on the console with a neat cascade effect. Nothing here is permanent.

--- trunk/Decoder.cpp
+++ trunk/Decoder.cpp
@@ -18,32 +18,32 @@ void Decoder::initHash()
 							{"C6","C#6","D6","D#6","E6"},
 							{"C5","C#5","D5","D#5","E5"} };	*/	
 	// EXPERT
-	notes["C8"] = B1;
-	notes["C#8"] = B2;
-	notes["D8"] = B3;
-	notes["D#8"] = B4;
-	notes["E8"] = B5;
+	notes["C7"] = pair<buttonsType,difficultyType>(B1,EXPERT);
+	notes["C#7"] = pair<buttonsType,difficultyType>(B2,EXPERT);
+	notes["D7"] = pair<buttonsType,difficultyType>(B3,EXPERT);
+	notes["D#7"] = pair<buttonsType,difficultyType>(B4,EXPERT);
+	notes["E7"] = pair<buttonsType,difficultyType>(B5,EXPERT);
 	
 	// MEDIUM
-	notes["C7"] = B1;
-	notes["C#7"] = B2;
-	notes["D7"] = B3;
-	notes["D#7"] = B4;
-	notes["E7"] = B5;
+	notes["C6"] = pair<buttonsType,difficultyType>(B1,MEDIUM);
+	notes["C#6"] = pair<buttonsType,difficultyType>(B2,MEDIUM);
+	notes["D6"] = pair<buttonsType,difficultyType>(B3,MEDIUM);
+	notes["D#6"] = pair<buttonsType,difficultyType>(B4,MEDIUM);
+	notes["E6"] = pair<buttonsType,difficultyType>(B5,MEDIUM);
 	
 	// EASY
-	notes["C6"] = B1;
-	notes["C#6"] = B2;
-	notes["D6"] = B3;
-	notes["D#6"] = B4;
-	notes["E6"] = B5;
+	notes["C5"] = pair<buttonsType,difficultyType>(B1,EASY);
+	notes["C#5"] = pair<buttonsType,difficultyType>(B2,EASY);
+	notes["D5"] = pair<buttonsType,difficultyType>(B3,EASY);
+	notes["D#5"] = pair<buttonsType,difficultyType>(B4,EASY);
+	notes["E5"] = pair<buttonsType,difficultyType>(B5,EASY);
 	
 	// SUPER EASY
-	notes["C5"] = B1;
-	notes["C#5"] = B2;
-	notes["D5"] = B3;
-	notes["D#5"] = B4;
-	notes["E5"] = B5;
+	notes["C4"] = pair<buttonsType,difficultyType>(B1,SUPEREASY);
+	notes["C#4"] = pair<buttonsType,difficultyType>(B2,SUPEREASY);
+	notes["D4"] = pair<buttonsType,difficultyType>(B3,SUPEREASY);
+	notes["D#4"] = pair<buttonsType,difficultyType>(B4,SUPEREASY);
+	notes["E4"] = pair<buttonsType,difficultyType>(B5,SUPEREASY);
 }	
 
 
@@ -65,5 +65,13 @@ string Decoder::note_from_int(int note_number)
 buttonsType Decoder::whatButton( string note )
 {
 	
-	return notes[note];	
+	return notes[note].first;	
+}
+
+buttonsType Decoder::whatButton( string note, difficultyType difficulty )
+{
+	if( notes[note].second == difficulty )
+		return notes[note].first;
+	else
+		return NIL;
 }
--- trunk/main.cpp
+++ trunk/main.cpp
@@ -1,19 +1,30 @@
 #include "smf.h"
 
+#include <stdlib.h>
 #include <string>
 #include <map>
-#include <sys/time.h>
 #include <iostream>
 using namespace std;
+#include <sys/time.h>
+#include <pthread.h>
+
 
 #include "Decoder.h"
 
 
 #define ONE_BILLION 1000000000
+#define SCREENY 50
+#define DEBUG 0
 
+#define DIFFICULTY EXPERT
 
 Decoder decoder;
 
+int mspqn;
+string defaultFile = "example.mid";
+bool update = false;
+int keys[5] = {0};
+int screen[SCREENY][5] = {{0}};
 
 
 void double_to_timespec(double diff_time, timespec *timer)
@@ -21,67 +32,138 @@ void double_to_timespec(double diff_time, timespec *timer)
 	timer->tv_sec	= (long int)diff_time;
 	diff_time -= timer->tv_sec;
 	
-	timer->tv_nsec	= diff_time * ONE_BILLION;
+	timer->tv_nsec	= diff_time * ONE_BILLION; 
 	//printf("double: %f\n", diff_time);
 }
 
-int main(int argc, char *argv[])
+void decodeEvent( smf_event_t *event )
+/* decodes what button is updates it in keys[] vector */
+{
+	string note = decoder.note_from_int( event->midi_buffer[1] );
+
+	int noteButton = decoder.whatButton( note, DIFFICULTY );
+	
+	//cout<<note<<" -> "<<noteButton<<endl;
+	
+	if(noteButton != NIL)
+		switch( event->midi_buffer[0] & 0xF0 )
+		{
+			case 0x80: //NOTE OFF"
+				keys[noteButton] = 0;
+				//cout << noteButton << " OFF" << endl;
+				if(DEBUG)
+					cout << note << " OFF" << endl;
+				break;
+
+			case 0x90: //NOTE ON"
+				keys[noteButton] = 1;
+				//cout << noteButton << " ON" << endl;
+				if(DEBUG)
+					cout << note << " ON" << endl;
+				break;
+		}
+	
+}
+
+static void *interpreter(void *argument)
+/* reads the mid file synchronously and calls decodeEvent() for every event */
 {
 	smf_t *smf;
-	smf_event_t *nextEvent, *actualEvent;
+	smf_event_t *event2, *event;
 	double diff_time;
-	
 	timespec tempo;
 	
-	
-	int terminou = 0;
-	if( argc>1 )
-		smf = smf_load(argv[1]);
-	else {
-		cout << "No argument passed, opening 'notes.mid' by default." << endl << endl;
-		smf = smf_load("notes.mid");
-	}
+	bool eof = false;
+	smf = smf_load( defaultFile.c_str() );
 		
 	if (smf == NULL)
-		return 1;
+		return NULL;
 	
-	//if((event = smf_get_next_event(smf)) != NULL)
-		//printf("%s\t %d\t %d\t %f\n", smf_event_decode(event), event->time_pulses, event->delta_time_pulses, event->time_seconds);
+	if( (event = smf_get_next_event(smf)) != NULL )
+		if( smf_event_is_metadata(event) ) {
+			mspqn = (event->midi_buffer[3] << 16) + (event->midi_buffer[4] << 8) + event->midi_buffer[5];
+			printf("%dms per quarter note\n", mspqn);
+		}
 		
 
-	while (!terminou) {
-		nextEvent = actualEvent;
-		if( (actualEvent = smf_get_next_event(smf)) == NULL) {
-			terminou = 1;
+	while (!eof) {
+		event2 = event;
+		if( (event = smf_get_next_event(smf)) == NULL) {
+			eof = true;
 			break;
 		}
-		if ( (diff_time = (actualEvent->time_seconds - nextEvent->time_seconds)) ) {
+		if ( (diff_time = (event->time_seconds - event2->time_seconds)) ) {
 			double_to_timespec(diff_time, &tempo);
 			nanosleep(&tempo, NULL);
 		}
 		
-		//printf("%s\t %d\t %d\t %f\n", smf_event_decode(event), event->time_pulses, event->delta_time_pulses, event->time_seconds);
+		decodeEvent(event);
 		
-
-		string note = decoder.note_from_int( actualEvent->midi_buffer[1] );
-		int noteButton = decoder.whatButton( note );
-				
-		switch( actualEvent->midi_buffer[0] & 0xF0 )
-		{
-			case 0x80: //means it's a "note off"
-				cout << noteButton << " OFF" << endl;
-				break;
-
-			case 0x90: //means it's a "note onf"
-				cout << noteButton << " ON" << endl;
-				break;
-		}
-		
+		//printf("%s\t %d\t %d\t %f\n", smf_event_decode(event), event->time_pulses, event->delta_time_pulses, event->time_seconds);		
 	}
 	
-	cout << endl << "End of file" << endl;
+	cout << endl << "End of file." << endl;
 	
 	smf_delete(smf);
 		
-	return 0;
+	return 0;	
+}
+
+static void *drawer(void *argument) 
+{
+  if(!DEBUG)
+	while(1)
+	{
+		usleep(mspqn/4); // "ms per quarternote" divided by 4 (just a number for a good resolution)
+		system("clear"); 
+		
+		// if in the last update the note has been turned ON
+		//  and wasnt turned OFF yet, changes its code to 2,
+		//  which will be represented with an "|" in the screen.
+		for(int i=0; i<5; i++)
+			if(keys[i]==1 && screen[0][i]==1)
+				keys[i]=2;
+				
+		// propagates the lines of the matrix	
+		for(int lin=SCREENY-1; lin>0; lin--)
+			for(int col=0; col<5; col++)
+				screen[lin][col] = screen[lin-1][col];
+				
+		// updates the first line of the matrix with the actual configuration		
+		for(int i=0; i<5; i++)
+			screen[0][i] = keys[i];
+			
+		//prints the matrix		
+		for(int lin=0; lin<SCREENY; lin++)
+		{
+			for(int col=0; col<5; col++)
+			{
+				switch(screen[lin][col]){
+					case 0: cout<<" "; break;
+					case 1: cout<<"O"; break;						
+					case 2: cout<<"|"; break;
+				}
+				cout<<"  ";
+			}
+			cout<<endl;
+		}
+	}
+		
+	return NULL;	
+}
+
+int main(int argc, char *argv[])
+{
+	cout<<"Press enter to start"<<endl; getchar();
+	
+	pthread_t threads[2];
+	int arg = 1;
+
+	pthread_create(&threads[0], NULL, interpreter, (void *) arg);
+	pthread_create(&threads[1], NULL, drawer, (void *) arg);
+	
+	/* wait for all threads to complete */
+	pthread_join(threads[0], NULL);
+	pthread_join(threads[1], NULL);
+
 }
