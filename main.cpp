f7e701c87d90e01db05cf0e3925ce5dc41902ad1
ninja.dalbem <ninja.dalbem@9ff2b497-3b3b-2cb2-345e-040a1e5cde19>
2010-10-27 01:05:01 +0000

    Created registration of music in the memory. It's working very well, only the drawer isn't behaving correctly. The problem is with the mothod i'm using to get the real time. It works fine if used in the "updater" function, but in the "drawer" function time passes much slower.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,45 @@
-#include "smf.h"
-
 #include <stdlib.h>
 #include <string>
 #include <map>
 #include <iostream>
+#include <vector>
 using namespace std;
-#include <sys/time.h>
+#include <time.h>
 #include <pthread.h>
 
+#include <irrKlang.h>
+#include "smf.h"
 
 #include "Decoder.h"
 
 
 #define ONE_BILLION 1000000000
-#define SCREENY 50
+#define SCREEN_Y 50
 #define DEBUG 0
 
-#define DIFFICULTY EXPERT
+
+enum note//types of notes of the screen
+{
+	STRIKE, HOLD, NOTHING
+};
+
+enum eventType
+{
+	ON, OFF, STAY
+};
+
+typedef struct 
+{
+	eventType buttons[5];
+	double time; //seconds
+} musicEvent;
+
 
 Decoder decoder;
-
-int mspqn;
-string defaultFile = "example.mid";
-bool update = false;
-int keys[5] = {0};
-int screen[SCREENY][5] = {{0}};
+vector<musicEvent> aMusic;
+string defaultFile = "example.mid", songFile = "example.ogg";
+int mspqn; //ms per quarter note
+note screen[SCREEN_Y][5] = {{NOTHING}};
 
 
 void double_to_timespec(double diff_time, timespec *timer)
@@ -36,117 +51,173 @@ void double_to_timespec(double diff_time, timespec *timer)
 	//printf("double: %f\n", diff_time);
 }
 
-void decodeEvent( smf_event_t *event )
+void decodeMidiEvent( smf_event_t *event )
 /* decodes what button is updates it in keys[] vector */
 {
-	string note = decoder.note_from_int( event->midi_buffer[1] );
-
-	int noteButton = decoder.whatButton( note, DIFFICULTY );
+	//getchar();
 	
-	//cout<<note<<" -> "<<noteButton<<endl;
+	buttonType button = decoder.whatButton( decoder.note_from_int(event->midi_buffer[1]), EXPERT );
+	//cout << smf_event_decode(event) << endl;
 	
-	if(noteButton != NIL)
-		switch( event->midi_buffer[0] & 0xF0 )
-		{
-			case 0x80: //NOTE OFF"
-				keys[noteButton] = 0;
-				//cout << noteButton << " OFF" << endl;
-				if(DEBUG)
-					cout << note << " OFF" << endl;
-				break;
-
-			case 0x90: //NOTE ON"
-				keys[noteButton] = 1;
-				//cout << noteButton << " ON" << endl;
-				if(DEBUG)
-					cout << note << " ON" << endl;
-				break;
+	if(button != NIL)  { // decoded the event successfully
+		musicEvent newEvent;
+		
+		newEvent.time = event->time_seconds;
+		//cout << newEvent.time << ": " << button << endl;
+		
+		if( aMusic.size()>0 && aMusic.back().time == newEvent.time ) { // this event is concorrent to the last, so we merge them
+			//cout<<"MERGE!"<<endl;
+			switch( event->midi_buffer[0] & 0xF0 ) {
+				case 0x80: aMusic.back().buttons[button] = OFF; break;
+				case 0x90: aMusic.back().buttons[button] = ON; break;
+			}
 		}
-	
+		else {
+			// initializes buttons states
+			for(int i=0; i<5; i++)
+				newEvent.buttons[i] = STAY;	
+				
+			switch( event->midi_buffer[0] & 0xF0 ) {
+				case 0x80: newEvent.buttons[button] = OFF; break;
+				case 0x90: newEvent.buttons[button] = ON; break;
+			}			
+		
+			// put this new event to the music events list
+			aMusic.push_back(newEvent);	
+		}
+	}
 }
 
-static void *interpreter(void *argument)
-/* reads the mid file synchronously and calls decodeEvent() for every event */
+int readMidi()
+/* reads the mid file synchronously and calls decodeMidiEvent() for every event */
 {
 	smf_t *smf;
-	smf_event_t *event2, *event;
-	double diff_time;
-	timespec tempo;
+	smf_event_t *event;
 	
 	bool eof = false;
 	smf = smf_load( defaultFile.c_str() );
 		
-	if (smf == NULL)
-		return NULL;
+	if (smf == NULL) 
+		return 1;
 	
-	if( (event = smf_get_next_event(smf)) != NULL )
-		if( smf_event_is_metadata(event) ) {
-			mspqn = (event->midi_buffer[3] << 16) + (event->midi_buffer[4] << 8) + event->midi_buffer[5];
-			printf("%dms per quarter note\n", mspqn);
-		}
-		
-
 	while (!eof) {
-		event2 = event;
-		if( (event = smf_get_next_event(smf)) == NULL) {
+		event = smf_get_next_event(smf);
+		if( !event ) 
 			eof = true;
-			break;
+		else if( smf_event_is_metadata(event) ) {
+			//mspqn = (event->midi_buffer[3] << 16) + (event->midi_buffer[4] << 8) + event->midi_buffer[5];
+			//printf("%dms per quarter note\n", mspqn);
+			//cout << smf_event_extract_text(event) << endl;
 		}
-		if ( (diff_time = (event->time_seconds - event2->time_seconds)) ) {
-			double_to_timespec(diff_time, &tempo);
-			nanosleep(&tempo, NULL);
-		}
-		
-		decodeEvent(event);
+		else
+			decodeMidiEvent(event);
 		
 		//printf("%s\t %d\t %d\t %f\n", smf_event_decode(event), event->time_pulses, event->delta_time_pulses, event->time_seconds);		
 	}
 	
 	cout << endl << "End of file." << endl;
-	
 	smf_delete(smf);
 		
 	return 0;	
 }
 
-static void *drawer(void *argument) 
+static void *updater(void *argument) 
 {
-  if(!DEBUG)
-	while(1)
+	clock_t actualTime, baseTime = clock();
+	float musicTime;
+	
+	while( aMusic.size()>0 )
 	{
-		usleep(mspqn/4); // "ms per quarternote" divided by 4 (just a number for a good resolution)
+		actualTime = clock();
+		musicTime = (float) (actualTime - baseTime) / CLOCKS_PER_SEC;
+		//cout<<"actualTime - baseTime = "<<actualTime<<"-"<<baseTime<<" = "<<actualTime - baseTime<<endl;
+		//cout<<musicTime<<endl;
+		//cout<<aMusic[0].time<<endl;
+		if( musicTime > aMusic[0].time ) {
+			//cout << "EVENT LAUNCHED" << endl;
+			// updates the first line of the matrix with the actual configuration		
+			for(int i=0; i<5; i++)
+				switch(aMusic[0].buttons[i]) {
+					case ON:
+						screen[0][i] = STRIKE;
+						break;
+					case OFF:
+						screen[0][i] = NOTHING;
+						break;
+					case STAY:
+						screen[0][i] = screen[1][i];
+						break;
+				}
+				
+			aMusic.erase(aMusic.begin());
+		}		
+	}
+	
+	return NULL;
+}
+
+
+
+static void *drawer(void *argument) 
+{		
+	//prints the matrix		
+	for(int lin=0; lin<SCREEN_Y; lin++)
+		for(int col=0; col<5; col++)
+			screen[lin][col] = NOTHING;
+	
+	while( 1 )
+	{
+		usleep(10000);
 		system("clear"); 
 		
-		// if in the last update the note has been turned ON
-		//  and wasnt turned OFF yet, changes its code to 2,
-		//  which will be represented with an "|" in the screen.
-		for(int i=0; i<5; i++)
-			if(keys[i]==1 && screen[0][i]==1)
-				keys[i]=2;
+		clock_t actualTime, baseTime = clock();
+		float musicTime;
+	
+		actualTime = clock();
+		musicTime = (float) (actualTime - baseTime) / CLOCKS_PER_SEC;
+		//cout<<"actualTime - baseTime = "<<actualTime<<"-"<<baseTime<<" = "<<actualTime - baseTime<<endl;
+		cout<<musicTime<<endl;
+		cout<<aMusic[0].time<<endl;
+		if( musicTime > aMusic[0].time ) {
+			//cout << "EVENT LAUNCHED" << endl;
+			// updates the first line of the matrix with the actual configuration		
+			for(int i=0; i<5; i++)
+				switch(aMusic[0].buttons[i]) {
+					case ON:
+						screen[0][i] = STRIKE;
+						break;
+					case OFF:
+						screen[0][i] = NOTHING;
+						break;
+					case STAY:
+						screen[0][i] = screen[1][i];
+						break;
+				}
 				
+			aMusic.erase(aMusic.begin());
+		}				
+		
 		// propagates the lines of the matrix	
-		for(int lin=SCREENY-1; lin>0; lin--)
+		for(int lin=SCREEN_Y-1; lin>0; lin--)
 			for(int col=0; col<5; col++)
 				screen[lin][col] = screen[lin-1][col];
 				
-		// updates the first line of the matrix with the actual configuration		
-		for(int i=0; i<5; i++)
-			screen[0][i] = keys[i];
-			
 		//prints the matrix		
-		for(int lin=0; lin<SCREENY; lin++)
+		for(int lin=0; lin<SCREEN_Y; lin++)
 		{
 			for(int col=0; col<5; col++)
 			{
-				switch(screen[lin][col]){
-					case 0: cout<<" "; break;
-					case 1: cout<<"O"; break;						
-					case 2: cout<<"|"; break;
+				switch(screen[lin][col])
+				{
+					case NOTHING: cout<<" "; break;
+					case STRIKE: cout<<"O"; break;						
+					case HOLD: cout<<"|"; break;
 				}
 				cout<<"  ";
 			}
 			cout<<endl;
 		}
+		
 	}
 		
 	return NULL;	
@@ -154,16 +225,38 @@ static void *drawer(void *argument)
 
 int main(int argc, char *argv[])
 {
-	cout<<"Press enter to start"<<endl; getchar();
-	
-	pthread_t threads[2];
+	irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
+	irrklang::ISoundSource* oggMusic = soundEngine->addSoundSourceFromFile(songFile.c_str()); 
+
+	pthread_t thread[2];
 	int arg = 1;
-
-	pthread_create(&threads[0], NULL, interpreter, (void *) arg);
-	pthread_create(&threads[1], NULL, drawer, (void *) arg);
+	 
+	readMidi();
+	 
+	cout<<"YAY"<<endl;
+	 
+	cout<<"size: " <<aMusic.size()<<endl;
+	for(int i=0; i<aMusic.size(); i++) {
+		cout<<aMusic[i].time<<": ";
+		for(int k=0; k<5; k++)
+			switch(aMusic[i].buttons[k])
+			{
+				case ON: cout << "ON\t"; break;
+				case OFF: cout << "OFF\t"; break;
+				case STAY: cout << "STAY\t"; break;
+			}
+		cout<<endl;
+	}
 	
-	/* wait for all threads to complete */
-	pthread_join(threads[0], NULL);
-	pthread_join(threads[1], NULL);
-
+	cout<<"Press enter to start the music!"<<endl; getchar();
+	
+	soundEngine->play2D(oggMusic, true);
+	pthread_create(&thread[0], NULL, drawer, (void *) arg);
+	//pthread_create(&thread[1], NULL, updater, (void *) arg);
+	
+	// wait for all threads to complete
+	pthread_join(thread[0], NULL);
+	//pthread_join(thread[1], NULL);
+	
+	soundEngine->drop();
 }
