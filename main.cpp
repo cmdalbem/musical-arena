6bf22d4fb20cc274fb9672c1f7e48de1ca87e86e
ninja.dalbem <ninja.dalbem@9ff2b497-3b3b-2cb2-345e-040a1e5cde19>
2010-11-02 05:48:06 +0000

    Integrated with Irrlicht Engine. Created some more functions on main.cpp, hoping to make the code more legible. Because I'm still playing with things, but hope people can understand what I'm pretending to do. Separated what is the update  things from what are draw things. So now you can call one or another without conflicting anything, and use drawer() only for console debuging and updater() for it's real purpose.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 // main.cpp
 //
-// Most important file on musical-arena, since here we have the main function =D
-//
 
-#include "Decoder.h"
-#include "stone.h"
-#include "utils.h"
+#include <vector>
+#include <iostream>
+using namespace std;
 
-// Addicional Libraries
+#define HAVE_IRRKLANG 1 //comment me to disable irrklang!
+
 #ifdef HAVE_IRRKLANG
   #include <irrKlang.h>
 #endif
 #include "smf.h"
 
-// says what note is being read in each moment
-Decoder decoder;
+#include <irrlicht.h>
+using namespace irr;
+
+#include "Decoder.h"
+#include "stone.h"
+#include "utils.h"
+#include "eventReceiver.h"
+
+
+#define STONE_DELAY -2
+
+
+Decoder decoder; 
 
 // stores an entire MIDI file, so we don't need to keep it opened during too much time
 vector<musicEvent> theMusic;
@@ -25,9 +35,17 @@ vector<stone*>	stonesOnScreen[NUMBER_OF_FRETS];
 // indicates the end of the music for the keyboard polling function
 bool endOfMusic;
 
-string defaultFile = "example.mid", songFile = "example.ogg";
-int mspqn; //ms per quarter note
+std::string defaultFile = "example.mid", songFile = "example.ogg", guitarFile = "";
 note theScreen[SCREEN_Y][NUMBER_OF_FRETS];
+
+// Irrlicht-related globals
+MyEventReceiver receiver;
+IrrlichtDevice *device;
+video::IVideoDriver* driver;
+scene::ISceneManager* smgr;
+// stores the stones present on the screen
+vector<scene::ISceneNode*> nodes[NUMBER_OF_FRETS];
+
 
 void matrix_update()
 {
@@ -102,7 +120,7 @@ int readMidi()
 	return 0;	
 }
 
-static void* drawer(void *argument) 
+static void *updater(void *argument) 
 {
 	struct	timeval start;
 	double	musicTime;
@@ -112,24 +130,33 @@ static void* drawer(void *argument)
 	
 	while( !endOfMusic )
 	{
-		usleep(80000);
-		musicTime = time_diff(start) + 78;
+		// In order to do framerate independent movement, we have to know how long it was since the last frame
+        u32 then = device->getTimer()->getTime();
+		const u32 now = device->getTimer()->getTime();
+		const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
+		then = now;
+
+		musicTime = time_diff(start);// + 10;
 		
-		cout << "music time: "	   << musicTime		 << endl
-			 << "upcoming event: " << theMusic[0].time << endl;
+//		cout << "music time: "	   << musicTime		 << endl
+	//		 << "upcoming event: " << theMusic[0].time << endl;
 
-		matrix_update();
-
-		while( ((musicTime /*+ STONE_DELAY*/) > theMusic[0].time) && !endOfMusic) {
-			// updates the first line of the matrix with the actual configuration
-			switch(theMusic[0].type) {
-				case ON:	{	// definitely, the last event on this track was an OFF
-					// shows the note on the matrix
-					theScreen[0][theMusic[0].button] = STRIKE;
-					
+		while( ((musicTime + STONE_DELAY) > theMusic[0].time) && !endOfMusic) {
+			int track = buttonType_to_int(theMusic[0].button); //0~4
+			
+			switch(theMusic[0].type) { 
+				case ON:
+				{	
 					// creates a new stone
-					int track = buttonType_to_int(theMusic[0].button);
 					stone *newStone = new stone(theMusic[0], -1);
+					
+					// creates a new scene node
+					scene::ISceneNode *newNode = smgr->addSphereSceneNode();
+					newNode->setPosition( core::vector3df(track*10 - 25, //initial x
+														  70,			 //initial y
+														  0) );          //initial z
+					newNode->setScale( core::vector3df(0.1,0.1,0.1) );
+					nodes[track].push_back(newNode);
 
 					// puts the stone on the end of the queue, i.e., it is, now, the
 					// last stone to be destroyed
@@ -137,33 +164,37 @@ static void* drawer(void *argument)
 					
 					break;
 				}
-				case OFF:	{	// definitely, the last event on this track was an ON
-					// erases the note on the matrix
-					theScreen[0][theMusic[0].button] = NOTHING;
-					
-					int track = buttonType_to_int(theMusic[0].button);
+				case OFF:
+				{
 					int vector_size = stonesOnScreen[track].size();
 					
 					// sets the destroy_time of the last element of the vector of
 					// stones of the referred track.
 					if(vector_size > 0)
 						stonesOnScreen[track][vector_size-1]->destroy_time = theMusic[0].time;
-					else	{
-						puts ("received an OFF without an ON O.o");
-						exit (1);
-					}
+					else
+						puts ("received an OFF without an ON O.o"); //lol 8D
 					
 					break;
 				}
+				default:
+					break;
+
 			}
-			// avoiding segmentation faults =D
 			if(theMusic.size() > 0)
 				theMusic.erase(theMusic.begin());
 			else
 				endOfMusic = true;
 		}
 		
-		
+		// updates nodes positions
+		for (int ti = 0; ti < NUMBER_OF_FRETS; ti++)
+			for(unsigned int ni=0; ni<nodes[ti].size(); ni++) {
+				core::vector3df pos = nodes[ti][ni]->getPosition();
+				//pos.Y -= 300*frameDeltaTime;
+				pos.Y -= 0.00001;
+				nodes[ti][ni]->setPosition(pos);
+			}
 		
 		// desalocate the stones for which the time has already gone
 		// for each track
@@ -173,8 +204,60 @@ static void* drawer(void *argument)
 				// while we have stones AND it is time to destroy them
 				while ( (stonesOnScreen[i].size() > 0) &&
 						(stonesOnScreen[i][0]->destroy_time != -1) &&
-						(musicTime > stonesOnScreen[i][0]->destroy_time) )
-					stonesOnScreen[i].erase(stonesOnScreen[i].begin());
+						(musicTime > 10*stonesOnScreen[i][0]->destroy_time) )
+				
+						stonesOnScreen[i].erase(stonesOnScreen[i].begin());
+						//this is where we delete the node too (method drop())
+
+	}
+	
+	return NULL;	
+}
+
+static void* drawer(void *argument) 
+{
+	struct	timeval start;
+	double	musicTime;
+	
+	// this is needed because, while we are using drawer() and updater()
+	// 	at the same time, both are editing theMusic vector.
+	// we solve the problem simply creating a copy because there's no
+	//  apparent reason need for them to be synchronized.
+	vector<musicEvent> theMusicCopied = theMusic;
+
+
+	// get the time before starting the music (so we can know how much time passed in each note)
+	gettimeofday(&start, NULL);
+	
+	while( !endOfMusic )
+	{
+		usleep(80000);
+		system("clear");
+		musicTime = time_diff(start);
+		
+		cout << "music time: "	   << musicTime		 << endl
+			 << "upcoming event: " << theMusicCopied[0].time << endl;
+
+		matrix_update();
+
+		while( ((musicTime /*+ STONE_DELAY*/) > theMusicCopied[0].time) && !endOfMusic) {
+			// updates the first line of the matrix with the actual configuration
+			switch(theMusicCopied[0].type) {
+				case ON:
+					theScreen[0][theMusicCopied[0].button] = STRIKE;
+					break;
+				case OFF:	
+					theScreen[0][theMusicCopied[0].button] = NOTHING;		
+					break;
+				default: break;
+			}
+			
+			// avoiding segmentation faults =D
+			if(theMusicCopied.size() > 0)
+				theMusicCopied.erase(theMusicCopied.begin());
+			else
+				endOfMusic = true;
+		}
 		
 		matrix_print();
 	}
@@ -183,17 +266,27 @@ static void* drawer(void *argument)
 }
 
 void* fretting (void *arg)
+// Poll the keyboard testing if the player has pressed the right notes.
 {
-	system("stty cbreak");
-	
 	while(!endOfMusic)
-	{
-		// poll the keyboard testing if the player has pressed the right notes.
-		if(getchar() == 'a')
-			;
+	{		
+		// example of how to get handle keyboard keys with Irrlicht
+		/*
+		core::vector3df nodePosition = node->getPosition();
+		
+		// receiver is a global EventHandler
+		if(receiver.IsKeyDown(irr::KEY_KEY_W))
+				nodePosition.Y += MOVEMENT_SPEED * frameDeltaTime;
+		else if(receiver.IsKeyDown(irr::KEY_KEY_S))
+				nodePosition.Y -= MOVEMENT_SPEED * frameDeltaTime;
+
+		if(receiver.IsKeyDown(irr::KEY_KEY_A))
+				nodePosition.X -= MOVEMENT_SPEED * frameDeltaTime;
+		else if(receiver.IsKeyDown(irr::KEY_KEY_D))
+				nodePosition.X += MOVEMENT_SPEED * frameDeltaTime;
+
+		node->setPosition(nodePosition); */
 	}
-	
-	system("stty icanon");
 	
 	return NULL;
 }
@@ -202,24 +295,6 @@ void musa_init()
 {
 	readMidi();
 	
-	//inicializes the matrix
-	for(int lin=0; lin<SCREEN_Y; lin++)
-		for(int col=0; col<5; col++)
-			theScreen[lin][col] = NOTHING;
-}
-
-int main(int argc, char *argv[])
-{
-#ifdef HAVE_IRRKLANG
-	irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
-	irrklang::ISoundSource* oggMusic = soundEngine->addSoundSourceFromFile(songFile.c_str());
-#endif
-	
-	pthread_t thread[3];
-	int arg = 1;
-	
-	musa_init();
-	 
 	cout<<"MIDI parsed. This is your music:"<<endl;
 	for(unsigned int i=0; i<theMusic.size(); i++) {
 		cout<<theMusic[i].time<<" - "<<theMusic[i].button << ": ";
@@ -227,62 +302,112 @@ int main(int argc, char *argv[])
 		{
 			case ON: cout << "ON"; break;
 			case OFF: cout << "OFF"; break;
+			default: break;
 		}
 		cout<<endl;
 	}
-	for (int i = 0; i <  NUMBER_OF_FRETS; i++)
-		cout << "size of fret " << i << ": " << stonesOnScreen[i].size() << endl;
 		
-	cout<<"Press enter to start the music!"<<endl; getchar();
-
-#ifdef HAVE_IRRKLANG
-	soundEngine->play2D(oggMusic, true);
-#endif
+	//initializes the matrix
+	for(int lin=0; lin<SCREEN_Y; lin++)
+		for(int col=0; col<5; col++)
+			theScreen[lin][col] = NOTHING;
+			
 	endOfMusic = false;
-	pthread_create(&thread[0], NULL, drawer, (void *) arg);
-	pthread_create(&thread[2], NULL, fretting, (void *) arg);
-	
-	// wait for all threads to complete
-	pthread_join(thread[0], NULL);
-	//pthread_join(thread[1], NULL);
+}
 
-#ifdef HAVE_IRRKLANG
-	soundEngine->drop();
-#endif
+void initializeIrrlicht()
+{
+// Graphical engine initializing
+	device = createDevice( video::EDT_OPENGL, core::dimension2d<u32>(800,600), 32,
+												false, //fullscreen?
+												false, //used stencil buffer?
+												false, //use vsync?
+												&receiver //event receiver
+						);
+	device->setWindowCaption(L"Musical Arena (MusA): The Adventures of Lucy in the Enchanted Realm of Diamond Sky");
+
+	driver = device->getVideoDriver();
+	smgr = device->getSceneManager();
+	
+    // like the real game camera
+    ///*
+    smgr->addCameraSceneNode (
+		0,					  // Camera parent
+		core::vector3df(0, -50, -30), // Look from
+		core::vector3df(0, 1, 0), // Look to
+		1);						  // Camera ID
+	//*/
+		
+	// debugging camera
+	//smgr->addCameraSceneNode ( 0, core::vector3df(0, 0, -120), core::vector3df(0, 1, 0), 1);
+
+}
+
+int main(int argc, char *argv[])
+{
+	#ifdef HAVE_IRRKLANG
+		irrklang::ISoundEngine* soundEngine = irrklang::createIrrKlangDevice();
+		irrklang::ISoundSource* oggMusic = soundEngine->addSoundSourceFromFile(songFile.c_str());
+		irrklang::ISoundSource* guitar;
+		if( guitarFile.length()>0 )
+			guitar = soundEngine->addSoundSourceFromFile(guitarFile.c_str());
+		else
+			guitar = NULL;
+	#endif
+	
+	initializeIrrlicht();
+
+
+	// Game engine initializing
+	pthread_t thread[3];
+	int arg = 1;
+
+	musa_init();
+
+	//cout<<"Press enter to start the music!"<<endl; getchar();
+
+	#ifdef HAVE_IRRKLANG
+		soundEngine->play2D(oggMusic, true);
+		if(guitar)
+			soundEngine->play2D(guitar, true);
+	#endif
+	//pthread_create(&thread[0], NULL, drawer, (void *) arg);
+	pthread_create(&thread[1], NULL, fretting, (void *) arg);
+	pthread_create(&thread[2], NULL, updater, (void *) arg);
+	
+	// Irrklang Main Loop
+	int lastFPS = -1;
+	while(device->run()) {
+			driver->beginScene(true, true, video::SColor(255,113,113,133));
+
+			smgr->drawAll(); // draw the 3d scene
+			device->getGUIEnvironment()->drawAll(); // draw the gui environment (the logo)
+
+			driver->endScene();
+
+			int fps = driver->getFPS();
+
+			if (lastFPS != fps)
+			{
+					core::stringw tmp(L"Movement Example - Irrlicht Engine [");
+					tmp += driver->getName();
+					tmp += L"] fps: ";
+					tmp += fps;
+
+					device->setWindowCaption(tmp.c_str());
+					lastFPS = fps;
+			}
+	}
+	device->drop();
+	
+	
+	// wait for threads to complete
+	pthread_join(thread[0], NULL);
+	pthread_join(thread[1], NULL);
+	pthread_join(thread[2], NULL);
+
+	#ifdef HAVE_IRRKLANG
+		soundEngine->drop();
+	#endif
 	return 0;
 }
-
-
-
-//////////////////////////////
-static void *updater(void *argument) 
-{
-	clock_t actualTime, baseTime = clock();
-	float musicTime;
-	
-	while( theMusic.size()>0 )
-	{
-		actualTime = clock();
-		musicTime = (float) (actualTime - baseTime) / CLOCKS_PER_SEC;
-		//cout<<"actualTime - baseTime = "<<actualTime<<"-"<<baseTime<<" = "<<actualTime - baseTime<<endl;
-		//cout<<musicTime<<endl;
-		//cout<<theMusic[0].time<<endl;
-		while( musicTime > theMusic[0].time ) {
-			//cout << "EVENT LAUNCHED" << endl;
-			// updates the first line of the matrix with the actual configuration		
-			switch(theMusic[0].type) {
-				case ON:
-					theScreen[0][theMusic[0].button] = STRIKE;
-					break;
-				case OFF:
-					theScreen[0][theMusic[0].button] = NOTHING;
-					break;
-			}
-				
-			theMusic.erase(theMusic.begin());
-		}		
-	}
-	
-	return NULL;
-}
-
