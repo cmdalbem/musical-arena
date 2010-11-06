7f5e4c30ddeb3ae0b5bc5f04e4793c166b056f11
vaulttech <vaulttech@9ff2b497-3b3b-2cb2-345e-040a1e5cde19>
2010-11-06 19:40:19 +0000

    added get_fmod.sh script

---
+++ trunk/get_fmod.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+FOLDER=fmod
+FILE=$FOLDER.zip
+
+# Takes the fmod library name for your computer =D
+MYSYSTEM := $(shell uname -m)
+ifeq ($(MYSYSTEM), x86_64)
+URL="http://www.fmod.org/index.php/release/version/fmodapi43204linux64.tar.gz"
+else
+URL="http://www.fmod.org/index.php/release/version/fmodapi43204linux.tar.gz"
+endif
+
+
+echo "Downloading FMOD...\n"
+wget $URL -O $FILE
+tar zxvf $FILE
+
+echo "Done."
