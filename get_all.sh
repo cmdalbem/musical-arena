7f29584a59116869e62e12d32d4f05da2f38e29c
ninja.dalbem <ninja.dalbem@9ff2b497-3b3b-2cb2-345e-040a1e5cde19>
2010-11-06 20:31:53 +0000

    Fixed get_fmod

--- get_all.sh
+++ get_all.sh
@@ -3,5 +3,6 @@
 echo "Getting all libraries required..."
 
 ./get_smf.sh
-./get_irrklang.sh
 ./get_irrlicht.sh
+#./get_irrklang.sh
+./get_fmod.sh
--- get_fmod.sh
+++ get_fmod.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-FOLDER=fmod
-FILE=$FOLDER.zip
-
 # Takes the fmod library name for your computer =D
-MYSYSTEM := $(shell uname -m)
-ifeq ($(MYSYSTEM), x86_64)
-URL="http://www.fmod.org/index.php/release/version/fmodapi43204linux64.tar.gz"
+if [ `uname -m` == 'x86_64' ]
+then
+	URL='http://www.fmod.org/index.php/release/version/fmodapi43204linux64.tar.gz'
+	FOLDER=fmodapi43204linux64
 else
-URL="http://www.fmod.org/index.php/release/version/fmodapi43204linux.tar.gz"
-endif
-
+	URL='http://www.fmod.org/index.php/release/version/fmodapi43204linux.tar.gz'
+	FOLDER=fmodapi43204linux
+fi
+FILE=$FOLDER.tar.gz
 
 echo "Downloading FMOD...\n"
-wget $URL -O $FILE
-tar zxvf $FILE
+wget $URL
+gzip -dc $FILE | tar xf -
+
+mv $FOLDER fmod
+rm $FILE
 
 echo "Done."
