00f954c26872b890e7df226d06e9e01ac9d5f800
ninja.dalbem <ninja.dalbem@9ff2b497-3b3b-2cb2-345e-040a1e5cde19>
2010-12-21 23:48:43 +0000

    Forgot to add to repository.

---
+++ gui.cpp
@@ -0,0 +1,40 @@
+#include "gui.h"
+
+extern IVideoDriver *driver;
+extern IGUIEnvironment *env;
+extern IrrlichtDevice *device;
+extern SAppContext musaGui;
+extern ITexture	*mainMenuBg;
+
+void makeMainMenu()
+{
+	#define WINSIZEX SCREENX/6.
+	#define WINSIZEY SCREENY/2.7
+	
+	// background image
+	musaGui.mainBg = env->addImage( rect<s32>(position2di(0, 0),dimension2di(SCREENX,SCREENY)) );
+	musaGui.mainBg->setScaleImage(true);
+	musaGui.mainBg->setImage( mainMenuBg );	
+	
+	// main window
+	IGUIWindow *window = env->addWindow( recti(position2di(640, 100),dimension2di(WINSIZEX,WINSIZEY)), false, 0, 0, GUI_INITIAL_MENU );
+	window->setDraggable(false);
+	//window->setDrawBackground(false);
+	window->setDrawTitlebar(false);
+	musaGui.mainWindow = window;
+	
+	// load file buttons
+	env->addButton(rect<s32>(position2di(WINSIZEX/2-100/2, 50-32/2),dimension2di(100,32)), window, GUI_ID_LOAD_NOTES, L"Load MIDI", L"Loads a Fret's on Fire music file");
+	env->addButton(rect<s32>(position2di(WINSIZEX/2-100/2, 90-32/2),dimension2di(100,32)), window, GUI_ID_LOAD_MUSIC1, L"Load Song File 1", L"Loads the main song's audio file (usually all lines except guitar)");
+	env->addButton(rect<s32>(position2di(WINSIZEX/2-100/2, 130-32/2),dimension2di(100,32)), window, GUI_ID_LOAD_MUSIC2, L"Load Song File 2", L"Loads an optional secondary song's audio file (usually the guitar line)");
+	
+	// difficulty combobox
+	musaGui.box = env->addComboBox(rect<s32>(position2di(WINSIZEX/2-100/2, 195-20/2),dimension2di(100,20)), window);
+	musaGui.box->setEnabled(false);
+	// AI checkbox
+	musaGui.aiCheck = env->addCheckBox(true, rect<s32>(position2di(WINSIZEX/2-20/2-40, 220-20/2),dimension2di(20,20)), window);
+	env->addStaticText(L"Play against AI?", rect<s32>(position2di(WINSIZEX/2+25-100/2, 220-20/2),dimension2di(100,20)), false, true, window);
+	// start button
+	musaGui.startButton = env->addButton(rect<s32>(position2di(WINSIZEX/2-100/2, 250-32/2),dimension2di(100,32)), window, GUI_ID_START_BUTTON, L"Start", L"Starts the game");
+	musaGui.startButton->setEnabled(false);
+}
